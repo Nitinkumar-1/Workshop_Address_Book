2671b7f0b64a66d37ac96c47c75c21294c8d2fee
Nitinkumar-1 <nitinkumar766823@gmail.com>
2025-03-19 11:30:47 +0530

    Implemented RabbitMQ

---
+++ src/main/java/com/example/Workshop_Address_Book/service/RabbitMQListener.java
@@ -0,0 +1,21 @@
+
+package com.example.Workshop_Address_Book.service;
+
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Service;
+
+@Service
+public class RabbitMQListener {
+
+    @RabbitListener(queues = "UserQueue")
+    public void handleUserRegistration(String email) {
+        System.out.println("📧 Sending registration email to: " + email);
+
+    }
+
+    @RabbitListener(queues = "ContactQueue")
+    public void handleNewContact(String email) {
+        System.out.println("📞 New contact added: " + email);
+
+    }
+}
